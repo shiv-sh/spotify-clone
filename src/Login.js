9ff4f7334d319a6d65c0f95e4255d2e464c68ed8
sharmashivam9611@gmail.com <sharmashivam9611@gmail.com>
2020-10-15 14:19:55 +0530

    changes

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <div>
+            <h1>This is login component</h1>
+            <img src="spotify-logo.png" alt=""/>
+            {/* spotify logo */}
+            {/* Login with spotify button */}
+        </div>
+    )
+}
+
+export default Login
